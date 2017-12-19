adc5097aa87aa492c03a6b6d5a0baab0a9bdc845
Jacky Hermans <hermans_stevens@skynet.be>
2017-12-19 13:32:28 +0100

    Add project files.

---
+++ TDDTakenVeilingTest/VeilingTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDTakenVeiling;
+
+namespace TDDTakenVeilingTest
+{
+    [TestClass]
+    public class VeilingTest
+    {
+        [TestMethod]
+        public void NogGeenBodEnHoogsteBedragIsNul()
+        {
+            Assert.AreEqual(0m, new Veiling().HoogsteBod);
+        }
+
+        [TestMethod]
+        public void EersteBodIsHoogsteBod()
+        {
+            var veiling = new Veiling();
+            veiling.DoeBod(1000m);
+            Assert.AreEqual(1000m, veiling.HoogsteBod);
+        }
+        [TestMethod]
+        public void MeerdereBiedingenWaarnaHoogsteBodGelijkIsAanBedragVanDitBod()
+        {
+            var veiling = new Veiling();
+            veiling.DoeBod(1000m);
+            veiling.DoeBod(2000m);
+            veiling.DoeBod(750m);
+            Assert.AreEqual(2000m, veiling.HoogsteBod);
+        }
+    }
+}
