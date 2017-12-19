d75a09f6e1c64207197297cb3e006105549dc368
Jacky Hermans <hermans_stevens@skynet.be>
2017-12-19 11:40:24 +0100

    Add project files.

---
+++ TDDTakenPalindroom/Woord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTakenPalindroom
+{
+    public class Woord
+    {
+        private readonly string woord;
+
+        public Woord(string woord)
+        {
+            this.woord = woord;
+        }
+        public bool IsPalindroom()
+        {
+            var andersom = new string(woord.ToArray().Reverse().ToArray());
+            return woord == andersom;
+        }
+    }
+}
---
+++ TDDTakenPalindroomTest/WoordTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDTakenPalindroom;
+
+namespace TDDTakenPalindroomTest
+{
+    [TestClass]
+    public class WoordTest
+    {
+        [TestMethod]
+        public void NegenIsEenPalindroom()
+        {
+            Assert.IsTrue(new Woord("negen").IsPalindroom());
+        }
+        [TestMethod]
+        public void ZevenIsGeenPalindroom()
+        {
+            Assert.IsFalse(new Woord("zeven").IsPalindroom());
+        }
+        [TestMethod]
+        public void LegeStringIsEenPalindroom()
+        {
+            Assert.IsTrue(new Woord(string.Empty).IsPalindroom());
+        }
+        [TestMethod]
+        public void Slechts1CijferAlsStringIsEenPalindroom()
+        {
+            Assert.IsTrue(new Woord("1").IsPalindroom());
+        }
+        [TestMethod]
+        public void Slechts1LetterAlsStringIsEenPalindroom()
+        {
+            Assert.IsTrue(new Woord("e").IsPalindroom());
+        }
+        [TestMethod]
+        public void AlsStringIs66EenPalindroom()
+        {
+            Assert.IsTrue(new Woord("66").IsPalindroom());
+        }
+        [TestMethod]
+        public void AlsStringIs67GeenPalindroom()
+        {
+            Assert.IsFalse(new Woord("67").IsPalindroom());
+        }
+    }
+}
